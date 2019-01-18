1dbdf63ef56deb3a403aab98a64928b20aaca75e
lynndu <656848349@qq.com>
2019-01-18 18:08:15 +0800

    my_project

--- node_vue/crm/src/api/modules/user.js
+++ node_vue/crm/src/api/modules/user.js
@@ -2,4 +2,4 @@ import fetch from '@/api/config';
 
 export const login = data => fetch('/auth/login', data)
 
-export const download = data => fetch('/download', data, 'get')
+export const download = data => fetch('/download', {}, 'get')
